c628103188853cbc9b439cd8aa6d57c3ff716b65
andre <a.ekani@gmail.com>
2024-10-01 11:54:47 +0200

    Update vite condig

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,32 @@
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import preserveDirectives from 'rollup-preserve-directives';
 
 export default defineConfig({
   plugins: [
-    preserveDirectives(),
+    preserveDirectives() as Plugin,
     react(),
     svgr({ include: '**/*.svg' }),
-    dts({ include: 'src', exclude: ['**/*.stories.tsx'], insertTypesEntry: true }),
-    { ...libInjectCss(), enforce: 'pre' },
+    dts({ include: 'src', exclude: ['**/*.stories.tsx'], insertTypesEntry: true, rollupTypes: true }),
     tsconfigPaths(),
-    vanillaExtractPlugin(),
-    {
-      name: 'test',
-      generateBundle: (_, bundle) => {
-        console.log(Object.values(bundle));
-      },
-    },
+    vanillaExtractPlugin({ unstable_mode: 'emitCss'}),
   ],
   build: {
     copyPublicDir: false,
+    minify: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'ui',
-      formats: ['es'],
-      fileName: 'ui',
+      formats: ['es', 'cjs'],
+      fileName: '[name]',
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime', 'framer-motion', 'polished'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'framer-motion', 'polished', '@djeka07/utils', '@djeka07/hooks'],
       output: {
         preserveModules: false,
         inlineDynamicImports: false,
@@ -45,6 +36,8 @@ export default defineConfig({
           'react/jsx-runtime': 'react/jsx-runtime',
           'framer-motion': 'framer-motion',
           polished: 'polished',
+          '@djeka07/utils': '@djeka07/utils',
+          '@djeka07/hooks': '@djeka07/hooks',
         },
       },
     },
