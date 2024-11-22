a5e259953bca91047ba5115e9a0c2822a53cb415
Andre Ekbom <andreekbom@live.se>
2024-11-22 14:30:20 +0100

    Export impage props

--- src/components/atoms/medias/images/index.ts
+++ src/components/atoms/medias/images/index.ts
@@ -1,4 +1,4 @@
 import Image from './image';
-import { SrcSet, Orientation } from './image.props';
+import { SrcSet, Orientation, ImageProps } from './image.props';
 export { Image };
-export type { SrcSet, Orientation };
+export type { SrcSet, Orientation, ImageProps };
